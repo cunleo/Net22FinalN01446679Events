8a00c09a747755bde72105da92f4dc8a6bf3443c
OLAPOJU OLUWOLE <35400817+cunleo@users.noreply.github.com>
2020-08-14 21:38:42 -0400

    Add project files.

---
+++ Net22FinalN01446679Events/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Net22FinalN01446679Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
